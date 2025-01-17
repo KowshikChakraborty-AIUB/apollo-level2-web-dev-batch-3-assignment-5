0d204e290e047bb6e5176cb7c02e5b57c583acf4
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-01-17 23:57:24 +0600

    Project setup (using react, tailwind and shadcn)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-green-500">Meeting Room Booking System</h1>
+      <div className="text-center">
+        <Button variant={"destructive"} className="text-center">Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
