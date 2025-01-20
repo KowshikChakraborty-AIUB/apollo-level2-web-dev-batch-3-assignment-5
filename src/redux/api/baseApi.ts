312bda03921bea2e622a112925e505bd3264ee40
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-01-20 23:38:55 +0600

    set credentials: include

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApiQuery = fetchBaseQuery({
     baseUrl: "https://apollo-level2-web-dev-batch-3-assignment-3-github.vercel.app/",
 
-    // credentials: "include",
+    credentials: "include",
     // prepareHeaders: (headers, { getState }) => {
     //     const token = (getState() as RootState).auth;
 
